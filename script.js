c55ac88ebe5c75a38851e15f708d6ad890b4ac3e
Alena Prochazkova <alena.prochazkova@gmail.com>
2024-10-07 19:52:52 +0300

    prvni priklad

---
+++ script.js
@@ -0,0 +1,14 @@
+// 3 prednaska
+
+// const hodinovka = 300
+// const pocetHodin = prompt("Zadej pocet hodin zamestnance", "160")
+// const premie = prompt("Zadej mimoradnou odmenu v Kc")
+// const plat = (hodinovka * pocetHodin) + premie
+
+// document.body.innerHTML = "<p>Pepa Novak si vydelal: " + plat + " Kc.</p>"
+
+
+// Priklad Ockovani
+const jmeno = prompt("Zedej tvoje jmeno a prijmeni:");
+const vek = prompt("Tvuj vek:");
+document.body.innerHTML += "<p>" + jmeno + ", vek: " + vek + "</p>" ;
